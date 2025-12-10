73b7d9026140fb676981058746af30df4780cab5
Lucas-Steffen <lucasgabrielamorinsteffemsnp@gmail.com>
2025-12-09 21:49:54 -0400

    feat: create Header and sidebar for ignite app

--- rocketseat_project/src/App.jsx
+++ rocketseat_project/src/App.jsx
@@ -1,12 +1,28 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import styles  from './App.module.css'
+import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Lucas Gabriel" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus magnam harum nostrum asperiores enim quis corrupti repudiandae praesentium. Accusamus porro harum eligendi in mollitia voluptate asperiores dolores nam expedita laboriosam!"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+          <Sidebar />
+        <main>
+          <Post 
+              author="Lucas Gabriel" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus magnam harum nostrum asperiores enim quis corrupti repudiandae praesentium. Accusamus porro harum eligendi in mollitia voluptate asperiores dolores nam expedita laboriosam!"
+            />
+
+            <Post 
+              author="João Gabriel" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus magnam harum nostrum asperiores enim quis corrupti repudiandae praesentium. Accusamus porro harum eligendi in mollitia voluptate asperiores dolores nam expedita laboriosam!"
+            />
+        </main>
+      </div>
     </div>
   )
 }
