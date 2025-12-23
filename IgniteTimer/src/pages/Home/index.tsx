d5c422e30a0a61e299891c40aea58d1a1bdcdbdd
Lucas-Steffen <lucasgabriel.programador@gmail.com>
2025-12-22 20:11:11 -0400

    feat(Home): add Zod validation schema for form handling with react-hook-form integration

--- IgniteTimer/src/pages/Home/index.tsx
+++ IgniteTimer/src/pages/Home/index.tsx
@@ -7,9 +7,18 @@ import { CountdownContainer,
          StartCountdownButton, 
          TaskInput } from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod';
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60),
+})
 
 export function Home(){
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    });
 
     function handleCreateNewCycle(data: any){
         console.log(data)
